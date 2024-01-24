19809a6715658da074a7ae0d665ef383d549b4f7
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-01-24 12:17:13 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,9 @@
-
-
 public class Main {
  
 	public static void main(final String[] args) {
 		PalindromeChecker sim = new PalindromeChecker();
         	sim.tester();
 	}
-	public void setup(){
-		String lines[] = loadStrings("palindromes.txt");
-        	System.out.println("there are " + lines.length + " lines");
-  		for (int i=0; i < lines.length; i++) {
-    			if(palindrome(lines[i])==true){
-      				System.out.println(lines[i] + " IS a palindrome.");
-    			}
-    			else {
-      			System.out.println(lines[i] + " is NOT a palindrome.");
-			}
-  		}
-	}
-	public String reverse(String str)
-{
-    String sNew = new String();
-    //your code here
-    str = clean(str);
-    for (int i = str.length() - 1; i >= 0; i--){
-      sNew += str.substring(i, i + 1);
-    }
-    return sNew;
-}
-public String clean(String str){
-  String cleanStr = new String();
-  for (int i = 0; i < str.length(); i++){
-    if (Character.isLetter(str.charAt(i))){
-      cleanStr += str.substring(i, i + 1);
-    }
-  }
-  return cleanStr.toLowerCase();
-}
-}
+	
 
+}
