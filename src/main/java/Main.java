dc42ba4935886a90381a8a323addfe49a31c875a
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-01-24 12:13:05 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,33 +6,19 @@ public class Main {
 		PalindromeChecker sim = new PalindromeChecker();
         	sim.tester();
 	}
-	public void setup()
-{
-  String lines[] = loadStrings("palindromes.txt");
-  System.out.println("there are " + lines.length + " lines");
-  for (int i=0; i < lines.length; i++) 
-  {
-    if(palindrome(lines[i])==true)
-    {
-      System.out.println(lines[i] + " IS a palindrome.");
-    }
-    else
-    {
-      System.out.println(lines[i] + " is NOT a palindrome.");
-    }
-  }
-}
-public boolean palindrome(String word)
-{
-  //your code here
-  if (clean(word).equals(reverse(word))){
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-public String reverse(String str)
+	public void setup(){
+		String lines[] = loadStrings("palindromes.txt");
+        	System.out.println("there are " + lines.length + " lines");
+  		for (int i=0; i < lines.length; i++) {
+    			if(palindrome(lines[i])==true){
+      				System.out.println(lines[i] + " IS a palindrome.");
+    			}
+    			else {
+      			System.out.println(lines[i] + " is NOT a palindrome.");
+			}
+  		}
+	}
+	public String reverse(String str)
 {
     String sNew = new String();
     //your code here
@@ -51,6 +37,5 @@ public String clean(String str){
   }
   return cleanStr.toLowerCase();
 }
-
 }
 
